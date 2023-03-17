c28f95070fc7ab6aaefaf485a2504c6719069272
OKS1978 <122022412+OKS1978@users.noreply.github.com>
2023-03-17 21:36:01 +0300

    Update calculator.py

--- calculator.py
+++ calculator.py
@@ -2,11 +2,4 @@ class Calculator:
     def multiply(self, x, y):
         return x * y
 
-    def division(self, x, y):
-        return x / y
-
-    def subtrection(self, x, y):
-        return x - y
-
-    def adding(self, x, y):
-        return x + y
+   
